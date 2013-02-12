7af4ae8a3d1e3185be0410cfa49e644b24a4962e
chengpeng chen <cp@baysensors.com>
2013-02-11 20:28:51 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '81f52370feecf69735925be56295e39a927ec812dbd95be338c512ec1613ed50eb856e83630af5e57fb0123576b06c06ffff0b441ccc75b1b78cb6c4a2a2df0d'
